a8cee89d336e61d118a39ec32e32d72adfe32379
HimashiRathnayake <nikeshalarathnayake19@gmail.com>
2021-08-31 15:08:22 +0000

    Update colab_cross_validate

--- colab_cross_validate.py
+++ colab_cross_validate.py
@@ -32,13 +32,7 @@ comment_labels = pd.get_dummies(comment_labels).values
 print("Shape of all comments: ", padded_docs.shape)
 print("Shape of labels: ", comment_labels.shape)
 
-X_train, X_test, Y_train, Y_test = train_test_split(padded_docs, comment_labels, test_size=0.1, random_state=42,
-                                                    shuffle=True)
-x_train, x_val, y_train, y_val = train_test_split(padded_docs, comment_labels, test_size=0.1, random_state=42,
-                                                    shuffle=True)
-x_train, y_train = apply_oversampling(x_train, y_train);
-x_test = X_test
-y_test = Y_test
+
 
 print("Train lables shape: ", y_train.shape)
 
@@ -81,3 +75,98 @@ print("F1-Score: ", f1_score(labels, predictions, average='weighted'))
 print("Macro Precision: ", precision_score(labels, predictions, average='macro'))
 print("Macro Recall: ", recall_score(labels, predictions, average='macro'))
 print("Macro F1-Score: ", f1_score(labels, predictions, average='macro'))
+
+
+# Define per-fold score containers
+acc_per_fold = []
+precision_per_fold = []
+recall_per_fold = []
+f1_per_fold = []
+macro_f1_per_fold = []
+macro_precision_per_fold = []
+macro_recall_per_fold = []
+
+kfold = KFold(n_splits=FOLDS, shuffle=True)
+
+fold_no = 1
+inputs = padded_docs
+targets = comment_labels
+
+for train, test in kfold.split(inputs, targets):
+
+  X_train, X_test, Y_train, Y_test = train_test_split(padded_docs, comment_labels, test_size=0.1, random_state=42,
+                                                    shuffle=True)
+  x_train, x_val, y_train, y_val = train_test_split(X_train, Y_train, test_size=0.1, random_state=42,
+                                                      shuffle=True)
+  x_train, y_train = apply_oversampling(x_train, y_train);
+  x_test = X_test
+  y_test = Y_test
+
+  # Generate a print
+  print('------------------------------------------------------------------------')
+  print(f'Training for fold {fold_no} ...')
+
+  # Fit data to model
+  model = build_model()
+  model, his = Train_Model(model, inputs[train], targets[train], cross_validation=True)
+
+  # Generate generalization metrics
+  scores = model.evaluate(inputs[test], targets[test], verbose=0)
+
+  y_val_pred = model.predict(inputs[test])
+  y_val_pred_cat = (np.asarray(y_val_pred)).round()
+  y_val = targets[test]
+
+  # Get performance metrics(macro averages) after each fold
+  macro_f1, macro_precision, macro_recall = f1_score(y_val, y_val_pred_cat, average='macro'), precision_score(y_val, y_val_pred_cat, average='macro'), recall_score(y_val, y_val_pred_cat, average='macro')
+
+  print(f"""Score for fold {fold_no}:
+    {model.metrics_names[0]} of {scores[0]}; 
+    {model.metrics_names[1]} of {scores[1]*100}% ;
+    {model.metrics_names[2]} of {scores[2]*100}% ;
+    {model.metrics_names[3]} of {scores[3]*100}% ;
+    {model.metrics_names[4]} of {scores[4]*100}% ;
+    macro precision of {macro_precision*100}% ;
+    macro recall of {macro_recall*100}% ;
+    macro f1 of {macro_f1*100}% ;
+    """)
+  
+  loss_per_fold.append(scores[0])
+  acc_per_fold.append(scores[1])
+  precision_per_fold.append(scores[2])
+  recall_per_fold.append(scores[3])
+  f1_per_fold.append(scores[4])
+
+  macro_f1_per_fold.append(round(macro_f1, 6))
+  macro_precision_per_fold.append(round(macro_precision, 6))
+  macro_recall_per_fold.append(round(macro_recall, 6))
+
+  # Increase fold number
+  fold_no = fold_no + 1
+
+# == Provide average scores ==
+print('------------------------------------------------------------------------')
+print('Score per fold')
+for i in range(0, len(acc_per_fold)):
+  print('------------------------------------------------------------------------')
+  print(f"""> Fold {i+1} - 
+  Loss: {loss_per_fold[i]} - 
+  Accuracy: {acc_per_fold[i]}% - 
+  Precesion: {precision_per_fold[i]}% - 
+  Recall: {recall_per_fold[i]}% - 
+  F1: {f1_per_fold[i]}%
+  Macro Precision: {macro_precision_per_fold[i]}%
+  Macro Recall: {macro_recall_per_fold[i]}%
+  Macro F1: {macro_f1_per_fold[i]}%
+  """)
+print('------------------------------------------------------------------------')
+print('Average scores for all folds:')
+print(f'> Loss: {np.mean(loss_per_fold)}')
+print(f'> Accuracy: {np.mean(acc_per_fold)} (+- {np.std(acc_per_fold)})')
+print(f'> Precision: {np.mean(precision_per_fold)}')
+print(f'> Recall: {np.mean(recall_per_fold)}')
+print(f'> F1: {np.mean(f1_per_fold)}')
+print(f'> Macro Precision: {np.mean(macro_precision_per_fold)}')
+print(f'> Macro Recall: {np.mean(macro_recall_per_fold)}')
+print(f'> Macro F1: {np.mean(macro_f1_per_fold)}')
+print('------------------------------------------------------------------------')
