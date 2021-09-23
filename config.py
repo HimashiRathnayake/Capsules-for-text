2ee7b0d6fe4891402e64148a790339c463a941b7
HimashiRathnayake <nikeshalarathnayake19@gmail.com>
2021-09-23 06:15:09 +0000

    Update config

--- config.py
+++ config.py
@@ -13,7 +13,13 @@ class Config():
                  y_train=None,
                  x_test=None,
                  y_test=None,
-                 pretrain_vec=None):
+                 pretrain_vec=None,
+                 init_lr=0.001,
+                 dropout_ratio=0.8,
+                 epochs=10,
+                 batch_size=8,
+                 l2 = 0.002,
+                 optimizer = "Adam"):
         # self.args = args
         self.init_lr = 0.001
         self.batch_size = 8
@@ -21,7 +27,7 @@ class Config():
         self.l2 = 0.002
         self.routing = True
         self.num_classes = num_classes
-
+        self.optimizer = "Adam"
         self.sequence_length = seq_len
         self.vocab_size = vocab_size
         self.embedding_size = embedding_size
