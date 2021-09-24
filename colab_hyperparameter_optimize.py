ec6719eb221aa939828cb34c69589491d4085322
HimashiRathnayake <nikeshalarathnayake19@gmail.com>
2021-09-24 03:01:56 +0000

    Update colab_hyperparameter_optimize

--- colab_hyperparameter_optimize.py
+++ colab_hyperparameter_optimize.py
@@ -14,6 +14,7 @@ from colab_parameters import *
 import hyperopt
 from hyperopt import hp, tpe, fmin, STATUS_OK, Trials
 import warnings
+from csv import writer
 
 all_data = load_data()
 
@@ -37,14 +38,14 @@ np.random.seed(0)
 
 space = {
     # 'init_lr': hp.choice('init_lr', [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 0.95]), # 0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95
-    'dropout_ratio': hp.choice('dropout_ratio', [0.3, 0.5, 0.8]), 
+    'dropout_ratio': hp.choice('dropout_ratio', [0.8]), # 0.3, 0.5
     'batch_size': hp.choice('batch_size', [32]),
     'epochs': hp.choice('epochs', [50]),
-    'l2': hp.choice('l2', [0.001, 0.002, 0.01, 0.02]),
-    'optimizer': hp.choice('optimizer', ['SGD', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam'])
+    'l2': hp.choice('l2', [0.002]), # 0.001, 0.01, 0.02
+    'optimizer': hp.choice('optimizer', ['SGD']) # 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam'
 }
 
-x_train, x_test, y_train, y_test = train_test_split(padded_docs, comment_labels, test_size=VALIDATION_SPLIT*2, random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(padded_docs[:100], comment_labels[:100], test_size=VALIDATION_SPLIT*2, random_state=0)
 x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=VALIDATION_SPLIT, random_state=0)
 x_train, y_train = apply_oversampling(x_train, y_train);
 
@@ -83,6 +84,20 @@ def objective(params):
     print("Current Trial", ret)
     print("Completed Trials:", trials.results)
 
+    with open('/content/drive/Shareddrives/FYP/optimize/result.csv', 'a') as f_object:
+        writer_object = writer(f_object)
+        writer_object.writerow(["loss", "batch_size", "dropout_ratio", "epochs", "l2", "optimizer"])
+        writer_object.writerow([
+          ret["loss"], 
+          ret["params"]["batch_size"],
+          ret["params"]["dropout_ratio"],
+          ret["params"]["epochs"],
+          ret["params"]["l2"],
+          ret["params"]["optimizer"]
+          ])
+        f_object.close()
+
+
     return ret
 
 # Run optimization - Random search.
@@ -91,7 +106,7 @@ def tune_hyperparameres():
       fn = objective, 
       space = space, 
       algo = tpe.rand.suggest,
-      max_evals = 30, 
+      max_evals = 1, 
       trials = trials, 
       verbose = 0
   )
